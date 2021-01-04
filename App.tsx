d64691f3e3c9b4c5ad98eef35854a4e0f706aac5
kpose <ganihujude@gmail.com>
2021-01-04 17:17:03 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>home here</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
